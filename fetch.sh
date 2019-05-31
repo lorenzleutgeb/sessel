a8b4a7d76c5c005fed979ba153ea29980555fd0d
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2019-05-31 21:22:31 +0200

    Create fetch.sh

---
+++ fetch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+wget \
+  https://freewavesamples.com/files/Alesis-Fusion-Bass-C3.wav \
+  https://freewavesamples.com/files/Casio-CZ-5000-Human-Voice-C4.wav \
+  https://freewavesamples.com/files/Korg-TR-Rack-Tinkle-Bells-C6.wav \
+  https://freewavesamples.com/files/Cat-Meow.wav \
+  https://freewavesamples.com/files/Casio-MT-600-Typhoon-Sound.wav
