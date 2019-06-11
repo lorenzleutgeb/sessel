7d5a51869635f5df0a1d765b3e222c8674d62883
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2019-06-11 21:40:44 +0200

    Tuning...

--- sessel.py
+++ sessel.py
@@ -4,6 +4,8 @@ import pygame
 import sys
 import time
 
+from subprocess import call
+
 # To install dependencies, run the following (tested on Raspbian):
 # sudo apt-get install python-gpiozero python-pygame
 
@@ -20,6 +22,9 @@ PINS = [
     "GPIO24"  # PIN18
 ]
 
+print "Setting volume to 100%"
+call(["amixer", "-q", "sset", "'PCM'", "90%"])
+
 if sys.argv[0] != __file__ or len(sys.argv) < 2:
     print "Run this file as follows:\n\tpython " + __file__ + " [list of sound files separated by space]"
 
@@ -35,11 +40,38 @@ print "Initializing pygame..."
 pygame.init()
 pygame.mixer.init(channels=n)
 
-# Channels and Buttons (one per sound file)
+# Channels, Sounds and Buttons (one per sound file)
 ch = [None] * n
+so = [None] * n
 bt = [None] * n
 
+rewind_requested = False
+
+print "PIN\tFILE"
+for i in range(n):
+    print str(PINS[i]) + "\t" + args[i]
+    so[i] = pygame.mixer.Sound(args[i])
+
+def rewind_if_requested():
+    global rewind_requested
+    if rewind_requested:
+        rewind_requested = False
+        rewind()
+
+def rewind():
+    print "Rewinding"
+    for i in range(n):
+        ch[i].stop()
+        ch[i].play(so[i], loops=-1)
+        ch[i].set_volume(0)
+
+def request_rewind():
+    global rewind_requested
+    print "Rewind requested"
+    rewind_requested = True
+
 def pressed(i):
+    rewind_if_requested()
     print str(i) + " pressed"
     ch[i].set_volume(1)
 
@@ -47,15 +79,19 @@ def released(i):
     print str(i) + " released"
     ch[i].set_volume(0)
 
-print "PIN\tFILE"
-for i in range(n):
-    print str(PINS[i]) + "\t" + args[i]
-    ch[i] = pygame.mixer.Sound(args[i]).play(loops=-1)
-    bt[i] = gpiozero.Button(PINS[i])
-    ch[i].set_volume(0)
+rewind_button = gpiozero.Button("GPIO25") # PIN22
+rewind_button.when_pressed = request_rewind
 
+for i in range(n):
+    bt[i] = gpiozero.Button(PINS[i])
     bt[i].when_pressed  = functools.partial(pressed, i)
     bt[i].when_released = functools.partial(released, i)
+
+print "Sounds initialized."
+
+for i in range(n):
+    ch[i] = so[i].play(loops=-1)
+    ch[i].set_volume(0)
 
 print "---"
 
